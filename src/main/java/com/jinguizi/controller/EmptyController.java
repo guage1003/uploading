1c6d4485e6e1fc85744b28601e02c719541138ee
gua1003 <2905043565@qq.com>
2021-01-15 15:44:47 +0800

    刘鹏翔

---
+++ src/main/java/com/jinguizi/controller/EmptyController.java
@@ -0,0 +1,11 @@
+package com.jinguizi.controller;
+
+/**
+ * @Title: EmptyController
+ * @Description:
+ * @Auther: liupengxiang
+ * @Version: 1.0
+ * @create 2021/1/15  15:44
+ */
+public class EmptyController {
+}
